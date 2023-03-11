7c285a77b7778c33cc218735b88b19521efeb4b5
Tereskina <iv.kseni2@gmail.com>
2023-03-11 00:36:38 +0000

    Amend search by price in ads/views/ad.py

--- ads/views/ad.py
+++ ads/views/ad.py
@@ -47,19 +47,24 @@ class AdListView(ListAPIView):
             else:
                 search_query |= Q(author__location__name__icontains=location)
 
-        price_from = request.GET.get('price_from', None)
-        if price_from:
-            if search_query is None:
-                search_query = Q(price__gte=price_from)
-            else:
-                search_query &= Q(price__gte=price_from)
 
-        price_to = request.GET.get('price_to', None)
-        if price_to:
-            if search_query is None:
-                search_query = Q(price__lte=price_to)
-            else:
-                search_query &= Q(price__lte=price_to)
+        if price_from := request.GET.get('price_from', None):
+            self.queryset = self.queryset.filter(price__gte=price_from)
+        if price_to := request.GET.get('price_to', None):
+            self.queryset = self.queryset.filter(price__lte=price_to)
+        # price_from = request.GET.get('price_from', None)
+        # if price_from:
+        #     if search_query is None:
+        #         search_query = Q(price__gte=price_from)
+        #     else:
+        #         search_query &= Q(price__gte=price_from)
+        #
+        # price_to = request.GET.get('price_to', None)
+        # if price_to:
+        #     if search_query is None:
+        #         search_query = Q(price__lte=price_to)
+        #     else:
+        #         search_query &= Q(price__lte=price_to)
 
         if search_query:
             self.queryset = self.queryset.select_related('author').prefetch_related('category').filter(search_query).\
